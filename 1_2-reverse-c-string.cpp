3a04ae63652b552d2b8958049891bbaf037a3b75
Nate Kaiser <natejkaiser@gmail.com>
2017-09-12 14:06:54 -0500

    forgot to change description from carryover

--- 1_2-reverse-c-string.cpp
+++ 1_2-reverse-c-string.cpp
@@ -1,6 +1,6 @@
-// Cracking the Coding Interview, Problem 1.1
-// Implement an algorithm to determine if a string has all unique characters. What
-// if you can not use additional data structures?
+// Cracking the Coding Interview, Problem 1.2
+// Write code to reverse a C-Style String. (C-String means that “abcd” is
+// represented as five characters, including the null character.)
 
 #include <iostream>
 #include <string>
